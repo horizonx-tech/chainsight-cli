7e5a716cff1f46a69b5dbbf86535de0116ca3215
megared05 <05megared@gmail.com>
2023-06-09 17:26:12 +0900

    format all lib.rs (canister code)

--- initial_pj/artifacts/initial_pj_relayer/src/lib.rs
+++ initial_pj/artifacts/initial_pj_relayer/src/lib.rs
@@ -1,1 +1,42 @@
-use std :: str :: FromStr ; use chainsight_cdk_macros :: { manage_single_state , setup_func , timer_task_func , cross_canister_call_func , define_web3_ctx , define_transform_for_web3 , define_get_ethereum_address , monitoring_canister_metrics , did_export } ; use ic_web3 :: types :: { Address , U256 } ; monitoring_canister_metrics ! (60) ; define_web3_ctx ! () ; define_transform_for_web3 ! () ; manage_single_state ! ("target_addr" , String , false) ; manage_single_state ! ("target_canister" , String , false) ; setup_func ! ({ target_canister : String , target_addr : String , web3_ctx_param : Web3CtxParam }) ; define_get_ethereum_address ! () ; timer_task_func ! ("set_task" , "sync" , true) ; ic_solidity_bindgen :: contract_abi ! ("./__interfaces/Uint256Oracle.json") ; # [derive (Clone , Debug , candid :: CandidType , candid :: Deserialize)] pub struct StringValueWithTimestamp { pub value : String , pub timestamp : u64 , } type CallCanisterArgs = () ; type CallCanisterResponse = (StringValueWithTimestamp) ; cross_canister_call_func ! ("get_last_snapshot" , CallCanisterArgs , CallCanisterResponse) ; async fn sync () { let target_canister = candid :: Principal :: from_text (get_target_canister ()) . unwrap () ; let res = call_get_last_snapshot (target_canister , ()) . await ; if let Err (err) = res { ic_cdk :: println ! ("error: {:?}" , err) ; return ; } let datum = res . unwrap () ; Uint256Oracle :: new (Address :: from_str (& get_target_addr ()) . unwrap () , & web3_ctx () . unwrap ()) . update_state (U256 :: from_str (& datum . value) . unwrap ()) . await . unwrap () ; ic_cdk :: println ! ("ts={}, value={:?}" , datum . timestamp , datum . value) ; } did_export ! ("initial_pj_relayer") ;
+use chainsight_cdk_macros::{
+    cross_canister_call_func, define_get_ethereum_address, define_transform_for_web3,
+    define_web3_ctx, did_export, manage_single_state, monitoring_canister_metrics, setup_func,
+    timer_task_func,
+};
+use ic_web3::types::{Address, U256};
+use std::str::FromStr;
+monitoring_canister_metrics!(60);
+define_web3_ctx!();
+define_transform_for_web3!();
+manage_single_state!("target_addr", String, false);
+manage_single_state!("target_canister", String, false);
+setup_func ! ({ target_canister : String , target_addr : String , web3_ctx_param : Web3CtxParam });
+define_get_ethereum_address!();
+timer_task_func!("set_task", "sync", true);
+ic_solidity_bindgen::contract_abi!("./__interfaces/Uint256Oracle.json");
+#[derive(Clone, Debug, candid :: CandidType, candid :: Deserialize)]
+pub struct StringValueWithTimestamp {
+    pub value: String,
+    pub timestamp: u64,
+}
+type CallCanisterArgs = ();
+type CallCanisterResponse = (StringValueWithTimestamp);
+cross_canister_call_func!("get_last_snapshot", CallCanisterArgs, CallCanisterResponse);
+async fn sync() {
+    let target_canister = candid::Principal::from_text(get_target_canister()).unwrap();
+    let res = call_get_last_snapshot(target_canister, ()).await;
+    if let Err(err) = res {
+        ic_cdk::println!("error: {:?}", err);
+        return;
+    }
+    let datum = res.unwrap();
+    Uint256Oracle::new(
+        Address::from_str(&get_target_addr()).unwrap(),
+        &web3_ctx().unwrap(),
+    )
+    .update_state(U256::from_str(&datum.value).unwrap())
+    .await
+    .unwrap();
+    ic_cdk::println!("ts={}, value={:?}", datum.timestamp, datum.value);
+}
+did_export!("initial_pj_relayer");
--- initial_pj/artifacts/initial_pj_snapshot_icp/src/lib.rs
+++ initial_pj/artifacts/initial_pj_snapshot_icp/src/lib.rs
@@ -1,1 +1,39 @@
-use chainsight_cdk_macros :: { manage_single_state , setup_func , manage_vec_state , timer_task_func , cross_canister_call_func , monitoring_canister_metrics , did_export } ; monitoring_canister_metrics ! (60) ; manage_single_state ! ("target_canister" , String , false) ; setup_func ! ({ target_canister : String }) ; manage_vec_state ! ("snapshot" , Snapshot , true) ; timer_task_func ! ("set_task" , "execute_task" , true) ; # [derive (Clone , Debug , candid :: CandidType , candid :: Deserialize)] pub struct Snapshot { pub value : SnapshotValue , pub timestamp : u64 , } type SnapshotValue = StringValueWithTimestamp ; # [derive (Clone , Debug , candid :: CandidType , candid :: Deserialize)] pub struct StringValueWithTimestamp { pub value : String , pub timestamp : u64 , } type CallCanisterArgs = () ; type CallCanisterResponse = (StringValueWithTimestamp) ; cross_canister_call_func ! ("get_last_snapshot" , CallCanisterArgs , CallCanisterResponse) ; async fn execute_task () { let current_ts_sec = ic_cdk :: api :: time () / 1000000 ; let target_canister = candid :: Principal :: from_text (get_target_canister ()) . unwrap () ; let res = call_get_last_snapshot (target_canister , ()) . await ; if let Err (err) = res { ic_cdk :: println ! ("error: {:?}" , err) ; return ; } let datum = Snapshot { value : res . unwrap () . clone () , timestamp : current_ts_sec , } ; add_snapshot (datum . clone ()) ; ic_cdk :: println ! ("ts={}, value={:?}" , datum . timestamp , datum . value) ; } did_export ! ("initial_pj_snapshot_icp") ;
+use chainsight_cdk_macros::{
+    cross_canister_call_func, did_export, manage_single_state, manage_vec_state,
+    monitoring_canister_metrics, setup_func, timer_task_func,
+};
+monitoring_canister_metrics!(60);
+manage_single_state!("target_canister", String, false);
+setup_func!({ target_canister: String });
+manage_vec_state!("snapshot", Snapshot, true);
+timer_task_func!("set_task", "execute_task", true);
+#[derive(Clone, Debug, candid :: CandidType, candid :: Deserialize)]
+pub struct Snapshot {
+    pub value: SnapshotValue,
+    pub timestamp: u64,
+}
+type SnapshotValue = StringValueWithTimestamp;
+#[derive(Clone, Debug, candid :: CandidType, candid :: Deserialize)]
+pub struct StringValueWithTimestamp {
+    pub value: String,
+    pub timestamp: u64,
+}
+type CallCanisterArgs = ();
+type CallCanisterResponse = (StringValueWithTimestamp);
+cross_canister_call_func!("get_last_snapshot", CallCanisterArgs, CallCanisterResponse);
+async fn execute_task() {
+    let current_ts_sec = ic_cdk::api::time() / 1000000;
+    let target_canister = candid::Principal::from_text(get_target_canister()).unwrap();
+    let res = call_get_last_snapshot(target_canister, ()).await;
+    if let Err(err) = res {
+        ic_cdk::println!("error: {:?}", err);
+        return;
+    }
+    let datum = Snapshot {
+        value: res.unwrap().clone(),
+        timestamp: current_ts_sec,
+    };
+    add_snapshot(datum.clone());
+    ic_cdk::println!("ts={}, value={:?}", datum.timestamp, datum.value);
+}
+did_export!("initial_pj_snapshot_icp");
