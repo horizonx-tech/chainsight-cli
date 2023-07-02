ccf47eab6e87e24749e67ff0644b0fdf7dbf674f
megared05 <05megared@gmail.com>
2023-07-02 14:20:29 +0900

    update: exec command - run scripts

--- src/commands/exec.rs
+++ src/commands/exec.rs
@@ -1,8 +1,8 @@
-use std::{path::Path, fs, os::unix::prelude::PermissionsExt};
+use std::{path::Path, fs, os::unix::prelude::PermissionsExt, process::Command};
 
-use anyhow::{bail, Ok};
+use anyhow::{bail};
 use clap::{arg, Parser};
-use slog::{error, info, Logger};
+use slog::{error, info, Logger, debug};
 
 use crate::{lib::{environment::EnvironmentImpl, utils::{is_chainsight_project, PROJECT_MANIFEST_FILENAME}, codegen::{project::ProjectManifestData, components::{common::{ComponentTypeInManifest, ComponentManifest}, event_indexer::EventIndexerComponentManifest, snapshot::SnapshotComponentManifest, relayer::RelayerComponentManifest}}}, types::{ComponentType, Network}};
 
@@ -23,6 +23,7 @@ pub struct ExecOpts {
 }
 
 const GLOBAL_ERROR_MSG: &str = "Fail 'Exec' command";
+const ENTRYPOINT_SHELL_FILENAME: &str = "entrypoint.sh";
 
 pub fn exec(env: &EnvironmentImpl, opts: ExecOpts) -> anyhow::Result<()> {
     let log = env.get_logger();
@@ -66,6 +67,7 @@ pub fn exec(env: &EnvironmentImpl, opts: ExecOpts) -> anyhow::Result<()> {
         info!(log, r#"Skip to generate commands to call components"#);
     } else {
         // generate commands
+        info!(log, r#"Processing for commands generation"#);
         execute_to_generate_commands(log, &artifacts_path_str, opts.network, &component_data)?;
     }
 
@@ -73,7 +75,8 @@ pub fn exec(env: &EnvironmentImpl, opts: ExecOpts) -> anyhow::Result<()> {
         info!(log, r#"Skip to execute commands to components"#);
     } else {
         // execute commands
-        execute_commands()?;
+        info!(log, r#"Processing for commands execution"#);
+        execute_commands(log, &artifacts_path_str)?;
     }
 
     info!(
@@ -104,17 +107,25 @@ fn execute_to_generate_commands(log: &Logger, builded_project_path_str: &str, ne
         let mut perms = fs::metadata(&filepath)?.permissions();
         perms.set_mode(0o755);
         fs::set_permissions(&filepath, perms)?;
+
+        info!(
+            log,
+            r#"Script for Component "{}" generated successfully"#,
+            data.label()
+        );
     }
 
     // temp
     // - automatic relative path setting
-    let entrypoint_filepath = format!("{}/entrypoint.sh", &script_root_path_str);
+    let entrypoint_filepath = format!("{}/{}", &script_root_path_str, ENTRYPOINT_SHELL_FILENAME);
     let component_names = component_data.iter().map(|c| c.label().to_string()).collect::<Vec<String>>();
     let entrypoint_contents = format!(r#"#!/bin/bash
 script_dir=$(dirname "$(readlink -f "$0")")
-
 {}
-"#, component_names.iter().map(|label| format!(". \"$script_dir/components/{}.sh\"", label)).collect::<Vec<String>>().join("\n"));
+"#, component_names.iter().map(|label| format!(r#"
+echo "Run script for '{}'"
+. "$script_dir/components/{}.sh"
+"#, &label, &label)).collect::<Vec<String>>().join("\n"));
     fs::write(
         &entrypoint_filepath,
         entrypoint_contents
@@ -123,9 +134,28 @@ script_dir=$(dirname "$(readlink -f "$0")")
     perms.set_mode(0o755);
     fs::set_permissions(&entrypoint_filepath, perms)?;
 
-    Ok(())
+    info!(log, r#"Entrypoint Scriptgenerated successfully"#);
+
+    anyhow::Ok(())
 }
 
-fn execute_commands() -> anyhow::Result<()> {
-    Ok(())
+fn execute_commands(log: &Logger, builded_project_path_str: &str) -> anyhow::Result<()> {
+    info!(log, "Run scripts to execute commands for deployed components");
+    let cmd_string = format!("./scripts/{}", ENTRYPOINT_SHELL_FILENAME);
+    debug!(log, "Running command: `{}`", &cmd_string);
+    let output = Command::new(&cmd_string)
+        .current_dir(&builded_project_path_str)
+        .output()
+        .expect(&format!("failed to execute process: {}", &cmd_string));
+    let complete_msg = format!("Executed '{}'", &cmd_string);
+    if output.status.success() {
+        debug!(log, "{}", std::str::from_utf8(&output.stdout).unwrap_or(&"fail to parse stdout"));
+        info!(log, "{} successfully", complete_msg);
+    } else {
+        debug!(log, "{}", std::str::from_utf8(&output.stderr).unwrap_or(&"fail to parse stderr"));
+        error!(log, "{} failed", complete_msg);
+        bail!(GLOBAL_ERROR_MSG.to_string())
+    }
+
+    anyhow::Ok(())
 }
