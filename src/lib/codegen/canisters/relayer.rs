a3854055e89ca154583d09ea0131dfe0243b516f
hide-yoshi <hide-yoshi@users.noreply.github.com>
2023-07-10 17:42:47 +0900

    update relayer

--- src/lib/codegen/canisters/relayer.rs
+++ src/lib/codegen/canisters/relayer.rs
@@ -18,8 +18,9 @@ fn common_codes() -> proc_macro2::TokenStream {
     let outside_call_idents = generate_outside_call_idents(OutsideCallIdentsType::All);
     quote! {
         use std::str::FromStr;
-        use chainsight_cdk_macros::{manage_single_state, setup_func, timer_task_func, cross_canister_call_func, define_web3_ctx, define_transform_for_web3, define_get_ethereum_address, monitoring_canister_metrics, did_export};
+        use chainsight_cdk_macros::{manage_single_state, setup_func, init_in, timer_task_func, define_web3_ctx, define_transform_for_web3, define_get_ethereum_address, monitoring_canister_metrics, did_export};
         use ic_web3_rs::types::{Address, U256};
+        use chainsight_cdk::rpc::{CallProvider, Caller, Message};
 
         monitoring_canister_metrics!(60);
 
@@ -28,6 +29,7 @@ fn common_codes() -> proc_macro2::TokenStream {
         define_get_ethereum_address!();
 
         timer_task_func!("set_task", "sync", true);
+        init_in!();
     }
 }
 
@@ -37,7 +39,6 @@ fn custom_codes(manifest: &RelayerComponentManifest) -> anyhow::Result<proc_macr
     let method_identifier = CanisterMethodIdentifier::parse_from_str(&method.identifier)?;
 
     let method_ident = &method_identifier.identifier;
-    let call_method_ident = format_ident!("call_{}", method_ident);
 
     // from destination: about oracle
     let destination = &manifest.destination;
@@ -67,19 +68,22 @@ fn custom_codes(manifest: &RelayerComponentManifest) -> anyhow::Result<proc_macr
 
         type CallCanisterArgs = (#(#request_ty_idents),*);
         #call_canister_response_type_ident
-        cross_canister_call_func!(#method_ident, CallCanisterArgs, CallCanisterResponse);
 
         async fn sync() {
             let target_canister = candid::Principal::from_text(get_target_canister()).unwrap();
-            let res = #call_method_ident(
-                target_canister,
-                (#(#request_val_idents),*)
-            ).await;
-            if let Err(err) = res {
+            let call_result = CallProvider::new(proxy())
+            .call(Message::new::<CallCanisterArgs>((#(#request_val_idents),*), target_canister.clone(), #method_ident).unwrap())
+            .await;
+            if let Err(err) = call_result {
                 ic_cdk::println!("error: {:?}", err);
                 return;
             }
-            let datum = res.unwrap();
+            let val = call_result.unwrap().reply::<CallCanisterResponse>();
+            if let Err(err) = val {
+                ic_cdk::println!("error: {:?}", err);
+                return;
+            }
+            let datum = val.unwrap();
 
             #oracle_ident::new(
                 Address::from_str(&get_target_addr()).unwrap(),
