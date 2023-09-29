b0393904d10ed356a178bb80833a60ac70557d80
darroze <darroze@horizonx.tech>
2023-09-29 15:53:07 +0900

    update sdk

--- src/lib/codegen/templates.rs
+++ src/lib/codegen/templates.rs
@@ -16,8 +16,8 @@ hex = "0.4.3"
 
 ic-web3-rs = { version = "0.1.1" }
 ic-solidity-bindgen = { version = "0.1.5" }
-chainsight-cdk-macros = { git = "https://github.com/horizonx-tech/chainsight-sdk.git", rev = "d5505ae7109b8f586e4558165222c0b94eceb084" }
-chainsight-cdk = { git = "https://github.com/horizonx-tech/chainsight-sdk.git", rev = "d5505ae7109b8f586e4558165222c0b94eceb084" }
+chainsight-cdk-macros = { git = "https://github.com/horizonx-tech/chainsight-sdk.git", rev = "8b58f73dbf582ad87f030005b0c2fde00d06377c" }
+chainsight-cdk = { git = "https://github.com/horizonx-tech/chainsight-sdk.git", rev = "8b58f73dbf582ad87f030005b0c2fde00d06377c" }
 "#.to_string()
 }
 
