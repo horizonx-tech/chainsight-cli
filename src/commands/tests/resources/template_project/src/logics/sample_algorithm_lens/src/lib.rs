0e9dddfcc043a11b9daea8fd90cc3e1bddd07577
megared05 <05megared@gmail.com>
2023-10-13 15:01:39 +0900

    clean: remove resources for integration-test

--- src/commands/tests/resources/template_project/src/logics/sample_algorithm_lens/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use sample_algorithm_lens_accessors::*;
-#[derive(Clone, Debug, Default, candid :: CandidType, serde :: Deserialize, serde :: Serialize)]
-pub struct LensValue {
-    pub dummy: u64,
-}
-pub async fn calculate(targets: Vec<String>) -> LensValue {
-    let _result = get_sample_snapshot_indexer_icp(targets.get(0usize).unwrap().clone()).await;
-    todo!()
-}
--- src/commands/tests/resources/template_project/src/logics/sample_relayer/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub type CallCanisterResponse = String;
-pub type CallCanisterArgs = ();
-pub fn call_args() -> CallCanisterArgs {
-    ()
-}
-pub fn filter(_: &CallCanisterResponse) -> bool {
-    true
-}
