73fa679d62ab9d48a5c661fa59a69d5e3f15a6c9
hide-yoshi <hide-yoshi@users.noreply.github.com>
2023-10-23 15:51:54 +0900

    remove unused

--- test__e2e_template/src/canisters/sample_event_indexer/src/lib.rs
+++
@@ -1,1 +0,0 @@
-use chainsight_cdk_macros :: def_event_indexer_canister ; def_event_indexer_canister ! ("{\"common\":{\"monitor_duration\":60,\"canister_name\":\"sample_event_indexer\"},\"def\":{\"identifier\":\"Transfer\",\"abi_file_path\":\"./__interfaces/ERC20.json\"}}") ;
