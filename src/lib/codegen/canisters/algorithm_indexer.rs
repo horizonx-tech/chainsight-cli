bc44e0778f602f07a41b025d42ab77157c405128
megared05 <05megared@gmail.com>
2023-10-29 23:19:56 +0900

    bump sdk version

--- src/lib/codegen/canisters/algorithm_indexer.rs
+++ src/lib/codegen/canisters/algorithm_indexer.rs
@@ -10,7 +10,7 @@ use crate::{
     types::ComponentType,
 };
 
-// TODO: remove this. see: https://github.com/horizonx-tech/chainsight-sdk/blob/dfd601d33c39154fb2c46d3826b2f100961c34f5/chainsight-cdk-macros/src/canisters/algorithm_indexer.rs#L39
+// TODO: remove this. see: https://github.com/horizonx-tech/chainsight-sdk/blob/58ab4ccb27db2bcd63c7f0d8ff9ffe5acf884e87/chainsight-cdk-macros/src/canisters/algorithm_indexer.rs#L39
 fn input_type_ident(manifest: &AlgorithmIndexerComponentManifest) -> TokenStream {
     let event_struct = format_ident!("{}", &manifest.datasource.input.name);
     let source_type = manifest.datasource.source_type.clone();
