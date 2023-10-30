23c022e5320b91f606e4a8c5072711272253e5c4
megared05 <05megared@gmail.com>
2023-10-30 13:26:40 +0900

    Revert "fix: remove duplicated dependencies in algorithm_lens"
    This reverts commit bf1c690ed1e8d716fb0acfa062dc11c1463b52cd.

--- src/lib/codegen/components/algorithm_lens.rs
+++ src/lib/codegen/components/algorithm_lens.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use candid::Principal;
 use chainsight_cdk::config::components::CommonConfig;
@@ -128,11 +128,9 @@ impl ComponentManifest for AlgorithmLensComponentManifest {
     }
 
     fn dependencies(&self) -> Vec<String> {
-        let mut seen = HashSet::new();
         self.datasource
             .methods
             .iter()
-            .filter(|e| seen.insert(&e.id)) // remove duplicated
             .map(|e| e.id.clone())
             .collect()
     }
