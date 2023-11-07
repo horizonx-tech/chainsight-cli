a2018e33414deee44ed48a38c6b5cebd36ad459a
megared05 <05megared@gmail.com>
2023-11-07 12:44:49 +0900

    add: utils for codegen

--- src/lib/codegen/components/common.rs
+++ src/lib/codegen/components/common.rs
@@ -6,7 +6,6 @@ use std::{
 };
 
 use anyhow::bail;
-use chainsight_cdk::convert::candid::CanisterMethodIdentifier;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
@@ -300,28 +299,4 @@ impl ComponentTypeInManifest {
         let data = Self::load(component_manifest_path)?;
         Ok(data.metadata.type_)
     }
-}
-
-// Generate types.rs code using the type information in bindings
-pub fn generate_types_from_bindings(id: &str, identifier: &str) -> anyhow::Result<String> {
-    let identifier = CanisterMethodIdentifier::new(identifier)?;
-    let (args_ty, _) = identifier.get_types();
-
-    let mut codes = format!(
-        r#"use {}_bindings as bindings;
-pub type {} = bindings::{};
-"#,
-        id,
-        CanisterMethodIdentifier::RESPONSE_TYPE_NAME,
-        CanisterMethodIdentifier::RESPONSE_TYPE_NAME
-    );
-    if args_ty.is_some() {
-        codes += &format!(
-            r#"pub type {} = bindings::{};"#,
-            CanisterMethodIdentifier::REQUEST_ARGS_TYPE_NAME,
-            CanisterMethodIdentifier::REQUEST_ARGS_TYPE_NAME
-        );
-    }
-
-    Ok(codes)
 }
