e56e8f7168aa422587c067574ae16a910d3bc929
megared05 <05megared@gmail.com>
2023-11-13 22:57:42 +0900

    update: snapshot_indexer_icp - if lens_target, setup with lens_target

--- src/lib/codegen/scripts/snapshot_indexer_icp.rs
+++ src/lib/codegen/scripts/snapshot_indexer_icp.rs
@@ -13,22 +13,53 @@ use crate::{
     types::{ComponentType, Network},
 };
 
-fn generate_command_to_setup(id: &str, datasrc_id: &str, network: &Network) -> String {
+fn generate_command_to_setup(
+    id: &str,
+    datasrc_id: &str,
+    lens_targets: &[String],
+    network: &Network,
+) -> String {
     let target_canister = principal_or_resolver_str(datasrc_id, network);
+
+    let lens_target_canisters = lens_targets
+        .iter()
+        .map(|t| principal_or_resolver_str(t, network))
+        .collect::<Vec<String>>();
+
+    let lens_targets_arg = if lens_target_canisters.is_empty() {
+        "".to_string()
+    } else {
+        format!(
+            r#"vec {{ \"{}\" }},"#,
+            lens_target_canisters.join(r#"\"; \""#)
+        )
+    };
 
     format!(
         r#"dfx canister {} call {} setup "(
-    \"{}\"
+    \"{}\",
+    {}
 )""#,
         network_param(network),
         id,
-        target_canister
+        target_canister,
+        lens_targets_arg
     )
 }
 fn script_contents(manifest: &SnapshotIndexerICPComponentManifest, network: Network) -> String {
     let id = manifest.id().unwrap();
 
-    let setup_contents = generate_command_to_setup(&id, &manifest.datasource.location.id, &network);
+    let setup_contents = generate_command_to_setup(
+        &id,
+        &manifest.datasource.location.id,
+        &manifest.lens_targets.clone().map_or(vec![], |v| {
+            v.identifiers
+                .iter()
+                .map(|i| i.to_string())
+                .collect::<Vec<String>>()
+        }),
+        &network,
+    );
 
     let start_timer_contents = generate_command_to_set_task(
         &id,
