db98bd98631d0afc097236a014302c9e56c8c866
hide-yoshi <hide-yoshi@users.noreply.github.com>
2023-12-29 15:03:04 +0900

    custom converter

--- src/lib/codegen/canisters/relayer.rs
+++ src/lib/codegen/canisters/relayer.rs
@@ -2,7 +2,10 @@ use anyhow::ensure;
 use chainsight_cdk::{
     config::components::{RelayerConfig, LENS_FUNCTION_ARGS_TYPE},
     convert::candid::CanisterMethodIdentifier,
+    web3::ContractFunction,
 };
+use ethabi::{Param, ParamType};
+use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
 
 use crate::{
@@ -16,6 +19,77 @@ use crate::{
     },
     types::ComponentType,
 };
+const CALL_ARGS_STRUCT_NAME: &str = "ContractCallArgs";
+
+#[derive(Clone)]
+struct ContractCall {
+    contract_function: ContractFunction,
+}
+
+impl ContractCall {
+    fn new(contract_function: ContractFunction) -> Self {
+        Self { contract_function }
+    }
+
+    fn function(&self) -> &ContractFunction {
+        &self.contract_function
+    }
+
+    fn call_args(&self) -> Vec<Param> {
+        self.contract_function.call_args()
+    }
+
+    fn call_args_struct(&self) -> TokenStream {
+        let names: Vec<String> = self
+            .call_args()
+            .clone()
+            .into_iter()
+            .map(|arg| arg.name)
+            .collect();
+        let types: Vec<TokenStream> = self
+            .call_args()
+            .clone()
+            .into_iter()
+            .map(|arg| Self::kind_to_ty(arg.kind))
+            .collect();
+        let visibly = "pub";
+        quote! {
+            #[derive(Clone, Debug)]
+            pub struct #CALL_ARGS_STRUCT_NAME {
+                #(#visibly #names: #types),*
+            }
+            impl #CALL_ARGS_STRUCT_NAME {
+                pub fn new(#(#names: #types),*) -> Self {
+                    Self {
+                        #(#names),*
+                    }
+                }
+            }
+        }
+        .into()
+    }
+
+    fn kind_to_ty(p: ParamType) -> TokenStream {
+        match p {
+            ParamType::Address => quote! { ethabi::Address },
+            ParamType::Bytes => quote! { Vec<u8> },
+            ParamType::FixedBytes(_) => quote! { Vec<u8> },
+            ParamType::Uint(_) => quote! { ic_web3_rs::types::U256 },
+            ParamType::Int(_) => quote! { ic_web3_rs::types::U256 },
+            ParamType::Bool => quote! { bool },
+            ParamType::String => quote! { String },
+            ParamType::Array(i) => {
+                let inner = Self::kind_to_ty(*i);
+                quote! { Vec<#inner> }
+            }
+            ParamType::FixedArray(i, _) => {
+                let inner = Self::kind_to_ty(*i);
+                quote! { Vec<#inner> }
+            }
+            ParamType::Tuple(_) => quote! { Vec<ethabi::Token> },
+        }
+    }
+}
 
 pub fn generate_codes(manifest: &RelayerComponentManifest) -> anyhow::Result<String> {
     ensure!(
@@ -31,6 +105,34 @@ pub fn generate_codes(manifest: &RelayerComponentManifest) -> anyhow::Result<Str
     Ok(code.to_string())
 }
 
+fn custom_converter(manifest: &RelayerComponentManifest) -> TokenStream {
+    let config: RelayerConfig = manifest.clone().into();
+    let contract_function = ContractFunction::new(config.abi_file_path, config.method_name);
+
+    let contract_call_args_struct_ident = match contract_function.call_args().len() {
+        0 => quote! { () },
+        1 => quote! { () },
+        _ => {
+            let call = ContractCall::new(contract_function.clone());
+            let ident = call.call_args_struct();
+            quote! { #ident }
+        }
+    };
+    let converter_method = match contract_function.call_args().len() {
+        0 => quote! { () },
+        1 => quote! { () },
+        _ => quote! {
+            pub fn convert(_: &CallCanisterResponse) -> #CALL_ARGS_STRUCT_NAME {
+                todo!()
+            }
+        },
+    };
+    quote! {
+        #contract_call_args_struct_ident
+        #converter_method
+    }
+}
+
 pub fn generate_app(manifest: &RelayerComponentManifest) -> anyhow::Result<String> {
     let method = manifest.datasource.method.clone();
     let interface = if method.interface.is_some() {
@@ -39,7 +141,7 @@ pub fn generate_app(manifest: &RelayerComponentManifest) -> anyhow::Result<Strin
         get_did_by_component_id(&manifest.datasource.location.id)
     };
     let method_identifier = generate_method_identifier(&method.identifier, &interface)?;
-
+    let converter = custom_converter(manifest);
     let call_args_idents = if manifest.lens_targets.is_some() {
         if is_lens_with_args(method_identifier) {
             let id = manifest.id.clone().expect("id is not set");
@@ -65,7 +167,6 @@ pub fn generate_app(manifest: &RelayerComponentManifest) -> anyhow::Result<Strin
         if request_args_type.is_some() {
             let request_args_ident =
                 format_ident!("{}", CanisterMethodIdentifier::REQUEST_ARGS_TYPE_NAME);
-
             quote! {
                 pub type CallCanisterArgs = types::#request_args_ident;
                 pub fn call_args() -> CallCanisterArgs {
@@ -87,6 +188,10 @@ pub fn generate_app(manifest: &RelayerComponentManifest) -> anyhow::Result<Strin
         mod types;
         pub type CallCanisterResponse = types::#response_ident;
         #call_args_idents
+        #converter
+        pub fn call_args(_: &CallCanisterResponse) -> ContractCallArgs {
+            todo!()
+        }
         pub fn filter(_: &CallCanisterResponse) -> bool {
             true
         }
