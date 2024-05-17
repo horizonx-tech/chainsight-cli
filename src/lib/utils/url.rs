54278012ea3a9c69fda3d0c30d48037edb3b7521
megared05 <05megared@gmail.com>
2024-05-17 14:07:29 +0900

    update: utils for url - no async/await

--- src/lib/utils/url.rs
+++ src/lib/utils/url.rs
@@ -1,8 +1,11 @@
+use std::net::{IpAddr, Ipv6Addr, SocketAddr, ToSocketAddrs};
+
 use anyhow::Result;
+use serde::{Deserialize, Serialize};
 
 // Reference source: https://github.com/horizonx-tech/chainsight-backend/blob/c16741812c63ddf7cbe7519d12743c5988daf689/functions/internal/src/app/deploy_relayer/deploy_relayer.rs#L195-L236
 
-pub async fn is_supporting_ipv6_url(url_str: &str) -> Result<()> {
+pub fn is_supporting_ipv6_url(url_str: &str) -> Result<()> {
     let url = url_str.parse::<url::Url>()?;
     let scheme = url.scheme();
     if scheme != "https" {
@@ -14,27 +17,62 @@ pub async fn is_supporting_ipv6_url(url_str: &str) -> Result<()> {
     match host {
         url::Host::Ipv4(_) => anyhow::bail!("ipv4 address is not acceptable for RPC URL"),
         url::Host::Ipv6(_) => Ok(()),
-        url::Host::Domain(domain) => match is_ipv6_supported(domain).await {
-            Ok(_) => Ok(()),
-            Err(_) => anyhow::bail!("Ipv6 not supported"),
-        },
-    }
-}
-
-async fn is_ipv6_supported(domain: &str) -> Result<()> {
-    let ips = tokio::net::lookup_host(domain.to_owned() + ":443").await?;
-    for ip in ips {
-        match ip {
-            std::net::SocketAddr::V6(_) => return Ok(()),
-            _ => continue,
+        url::Host::Domain(domain) => {
+            if is_ipv6_supported(domain) {
+                Ok(())
+            } else {
+                anyhow::bail!("Ipv6 not supported")
+            }
         }
     }
-    anyhow::bail!("No IPv6 address found for domain: {}", domain)
 }
 
-pub async fn is_valid_rpc_url(url: &str) -> Result<()> {
-    let tr = web3::transports::Http::new(url)?;
-    let web = web3::Web3::new(tr);
-    let _ = web.eth().block_number().await?;
+fn is_ipv6_supported(domain: &str) -> bool {
+    let socket_addr = SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), 443);
+    let ips: Vec<SocketAddr> = domain
+        .to_socket_addrs()
+        .unwrap()
+        .filter(|addr| match addr {
+            SocketAddr::V6(_) => true,
+            _ => false,
+        })
+        .collect();
+    for ip in ips {
+        if ip == socket_addr {
+            return true;
+        }
+    }
+    false
+}
+
+#[derive(Serialize, Deserialize)]
+struct JsonRpcRequest {
+    method: String,
+    params: Vec<serde_json::Value>,
+    id: u64,
+    jsonrpc: String,
+}
+
+#[allow(dead_code)]
+#[derive(Deserialize)]
+struct JsonRpcResponse {
+    id: u64,
+    jsonrpc: String,
+    #[serde(rename = "result")]
+    block_number: String,
+}
+
+pub fn is_valid_rpc_url(url: &str) -> Result<()> {
+    let request = JsonRpcRequest {
+        method: "eth_blockNumber".to_string(),
+        params: Vec::new(),
+        id: 1,
+        jsonrpc: "2.0".to_string(),
+    };
+
+    let _ = ureq::post(url)
+        .set("Content-Type", "application/json")
+        .send_json(&request)?
+        .into_json::<JsonRpcResponse>()?;
     Ok(())
 }
